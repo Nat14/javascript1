7600fdeb9b6f9d2fbb05c0b522e5edb87ed4aadd
Nat14 <naryucha14@gmail.com>
2016-02-13 15:55:55 -0800

    add challenge details

--- challenge1.js
+++ challenge1.js
@@ -1,3 +1,26 @@
+// You are writing the user registration page for a secure web site.
+// On the registration page, the user has to enter a user ID and a password, which has to adhere to the to following criteria:
+
+
+// User ID and password cannot be the same
+// User ID and password have to be at least six characters long
+// User ID cannot contain the following characters: !#$
+// Password has to contain at least one of: !#$
+// Password cannot be "password"
+// Stretch goal:
+//
+// Password has to contain at least one digit
+// Super stretch goal:
+//
+// Password has to contain a lower and an uppercase letter (hint: use toUpperCase or toLowerCase)
+// Write a main method that
+//
+// First lets a user input both user ID and password,
+// Then create methods corresponding to criteria above to evaluate the user ID and password
+// Then output whether the combination the combination is acceptable or not.
+// Run the program and test it for all criteria listed above by inputting different values.
+// Hint: See how the javascript String class can help you.
+
 function isUserValid(str) {
   return str.length > 5 && !str.includes("!") && !str.includes("#")  && !str.includes("$") ;
 }
